b6e7f02daa16f97f7337df75fb92a84370b57258
JPavlenko <pavlenkojekaterina@gmail.com>
2021-08-02 03:15:12 +0100

    links fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
 import Training from './components/pages/Training';
 import Nutrition from './components/pages/Nutrition';
